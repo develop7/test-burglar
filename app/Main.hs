eb412e1c7286ebf3a7e949657abc840b68eabaad
Andrei Dziahel <develop7@develop7.info>
2020-06-30 19:39:52 +0300

    Works end to end

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
+import Data.List.Split (splitOn)
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Enter space separated room length and width:"
+  line <- getLine
+  let (m : n : _) = map read (words line) :: [Int]
+  putStrLn "Enter space separated alarm coordinates and sensivity, empty line to end (example: 2 4 3 means alarm on (2,4) of sensivity 3): "
+
+  alarms <- readAlarms []
+  let (getIn, getAway) = plan m n alarms
+  putStrLn $ "Okay, here's the plan:\n1. Get in.\n2. Move through these exact cells - " ++ show ((m - 1, 0) : getIn)
+  putStrLn $ "3. Grab the diamond\n4. Move towards the exit via these cells - " ++ show getAway
+  putStrLn "5. ??????\n6. PROFIT"
+  where
+    parseCoords line = do
+      let (x : y : strength : _) = map read $ words line :: [Int]
+      Alarm (x, y) strength
+    readAlarms alarms = do
+      input <- getLine
+      if input /= ""
+        then readAlarms (parseCoords input : alarms)
+        else return alarms
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,27 +1,29 @@
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Lib
-  ( someFunc,
-    distance,
-    triggerProbability,
+  ( distance,
+    triggerChance,
     fieldGraphFun,
-    plan
+    plan,
+    moveCost,
+    Alarm (..),
+    Coords,
   )
 where
 
-import Data.HashSet
 import Data.Graph.AStar (aStar)
-
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.HashSet (HashSet, fromList)
+import Debug.Trace (traceId, traceShow, traceShowId)
 
 type Coords = (Int, Int)
 
 distance :: Coords -> Coords -> Double
-distance (xFrom, yFrom) (xTo, yTo) = sqrt . realToFrac $ (xTo - xFrom) ^ 2 + (yTo - yFrom) ^ 2
+distance (xFrom, yFrom) (xTo, yTo) = sqrt . fromIntegral $ (xTo - xFrom) ^ 2 + (yTo - yFrom) ^ 2
 
-triggerProbability :: Double -> Double -> Double
-triggerProbability l strength = exp $ negate ((l ** 2) / (strength ** 2))
+triggerChance :: Double -> Int -> Double
+triggerChance l strength = exp $ negate ((l ** 2) / fromIntegral (strength ^ 2))
 
--- | Generate a "neighbors" function for a rectangle field of m × n square cells 
+-- | Generate a "neighbors" function for a rectangle field of m × n square cells
 fieldGraphFun :: Int -> Int -> (Coords -> HashSet Coords)
 fieldGraphFun m n = fg'
   where
@@ -48,16 +50,27 @@ fieldGraphFun m n = fg'
     -- inside field
     fg (x, y) = [(x - 1, y), (x, y - 1), (x + 1, y), (x, y + 1)]
 
-data Alarm = Alarm {position :: Coords, strength :: Int}
+data Alarm = Alarm {position :: Coords, strength :: Int} deriving (Show)
 
-moveCostFun :: [Alarm] -> Coords -> Coords -> Double
-moveCostFun alarms pos _ = 1.0
+moveCost :: [Alarm] -> Coords -> Coords -> Double
+moveCost alarms from to = alarmChanceTotal + 2.0 -- Add 2 to make sure the "distance" will be positive; negative distances cause path calculation to skew towards an less effective path for some reason
+  where
+    alarmChanceTotal = sum $ map (alarmChanceDelta from to) alarms
+    alarmChanceDelta :: Coords -> Coords -> Alarm -> Double
+    alarmChanceDelta from to Alarm {position, strength} =
+      triggerChance (distance position to) strength - triggerChance (distance position from) strength
 
-plan :: Int -> Int -> [Coords] -> [Coords]
+plan :: Int -> Int -> [Alarm] -> ([Coords], [Coords])
 plan m n alarms = do
-  let exitPos = (m-1, n-1)
-  let (xm, ym) = exitPos
-  let diamondPos = ( xm `div` 2, ym `div` 2) :: Coords
-  let Just gettingIn = aStar (fieldGraphFun m n) (moveCostFun []) (distance diamondPos) (diamondPos ==) (0,0)
-  let Just getaway = aStar (fieldGraphFun m n) (moveCostFun []) (distance exitPos) (exitPos ==) diamondPos
-  gettingIn ++ getaway
+  let endPos = (m - 1, n - 1)
+  let validAlarms = foldl alarmFilter [] alarms
+  let (xm, ym) = endPos
+  let diamondPos = (xm `div` 2, ym `div` 2) :: Coords
+  let Just gettingIn = aStar (fieldGraphFun m n) (moveCost validAlarms) (distance diamondPos) (diamondPos ==) (xm, 0)
+  let Just getaway = aStar (fieldGraphFun m n) (moveCost validAlarms) (distance (0, ym)) ((0, ym) ==) diamondPos
+  (gettingIn, getaway)
+  where
+    alarmFilter :: [Alarm] -> Alarm -> [Alarm]
+    alarmFilter acc a@(Alarm (x, y) s)
+      | x > 0, x < m, y > 0, y < n, s > 0 = a : acc
+      | otherwise = acc
